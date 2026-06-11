9c0d6702faedf022132d02ac028cb05235a41593
SamSwift <samueludofia94@gmail.com>
2026-06-12 00:09:14 +0100

    Fix:update

--- swift/env.py
+++ swift/env.py
@@ -10,6 +10,7 @@ CAL_API_KEY = os.getenv("CAL_API_KEY", None)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", None)
 LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY", None)
 REDIS_URI = os.getenv("REDIS_URI", None)
+DATABASE_URI = os.getenv("DATABASE_URI", None)
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", None)
 SUPABASE_DB_URI = os.getenv("SUPABASE_DB_URI", None)
 
@@ -18,7 +19,8 @@ required_env_vars = [
     "SUPABASE_ACCESS_TOKEN",
     "CAL_API_KEY",
     "OPENAI_API_KEY",
-    "SUPABASE_DB_URI"
+    "SUPABASE_DB_URI",
+    "DATABASE_URI"
 ]
 
 for var in required_env_vars:
